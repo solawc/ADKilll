402930a522a646ab42b96f01f05fca0c4fb23610
solawc <1225093964@qq.com>
2020-03-13 15:08:30 +0800

    修改了一些bug

--- ADKilll.bat
+++ ADKilll.bat
@@ -1,8 +1,10 @@
-del *.htm/s	   ::删除网页文件
-del *.txt/s        ::删除txt文件
-del *.OutJob/s     ::删除工程输出文件
-cd History /s      ::进入History文件夹
-del *.zip /s       ::删除History文件夹中的.zip
+del *.htm/s	   	
+del *.txt/s        	
+del *.OutJob/s     	
+del *.PrjPcbStructure/s
+del *.LOG/s
+cd History      	
+del *.zip /s       	
 
 
 exit
